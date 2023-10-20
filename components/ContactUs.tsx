9b0aaba317b21f31d6be506daffcec04c81f565d
Prince Lee (BB) <prince_lee.nna85@ymail.com>
2023-10-20 12:16:01 +0100

    Update ContactUs.tsx

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -91,7 +91,8 @@ export const ContactUs = () => {
               <Col lg="8">
                 <Card className="bg-gradient-secondary shadow">
                   <CardBody className="p-lg-5">
-                    <h4 className="mb-1">Want to work with me?</h4>
+                    <h4 className="mb-1">Your project success is my priority. </h4>
+                    <h4 className="mb-1">Let's discuss!</h4>
                     <p className="mt-0">
                       Reach out to me using the form below.
                     </p>
