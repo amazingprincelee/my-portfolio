1e006254d20c0a16d91856eca1397ca43898c3e7
Prince Lee <prince_lee.nna85@ymail.com>
2025-06-02 09:31:54 +0100

    fixed, error

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,12 +46,12 @@ export default function Home({
 //   githubProfileData: PropTypes.object.isRequired,
 // };
 
-export async function getStaticProps() {
-  const githubProfileData: GithubUserType = await fetch(
-    `https://api.github.com/users/${openSource.githubUserName}`
-  ).then((res) => res.json());
+// export async function getStaticProps() {
+//   const githubProfileData: GithubUserType = await fetch(
+//     `https://api.github.com/users/${openSource.githubUserName}`
+//   ).then((res) => res.json());
 
-  return {
-    props: { githubProfileData },
-  };
-}
+//   return {
+//     props: { githubProfileData },
+//   };
+// }
